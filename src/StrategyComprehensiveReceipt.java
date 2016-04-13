86d1ed022b08f6bd711e3f972c3bc4c039ef6de5
Nergal Issaie <nissaie@yahoo.com>
2016-04-12 21:59:53 -0700

    Initial classes based on the initial class diagram are created

---
+++ src/StrategyComprehensiveReceipt.java
@@ -0,0 +1,5 @@
+/**
+ * Created by cmpe 202 - group 4 on 4/12/16.
+ */
+public class StrategyComprehensiveReceipt {
+}
---
+++ src/UserAccount.java
@@ -0,0 +1,6 @@
+/**
+ * Created by cmpe 202 - group 4 on 4/12/16.
+ */
+public class UserAccount {
+
+}

b3dd974826b1bc49ca824dedf73ba817c2de5d5b
gregt101 <gregt101@gmail.com>
2016-04-21 23:42:42 -0700

    Add setter and getter for userID

--- src/UserAccount.java
+++ src/UserAccount.java
@@ -2,5 +2,12 @@
  * Created by cmpe 202 - group 4 on 4/12/16.
  */
 public class UserAccount {
-
+    String userID;
+    public void setUserID(String id) {
+        userID = id;
+    }//setUserID
+    
+    public String getUserId() {
+        return userID;
+    }//getUserId
 }

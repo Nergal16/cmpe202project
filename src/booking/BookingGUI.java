a67cad0d848854c23dd971fd24c797e85bdd9ac2
MUKUL821 <mukul.bajpai0821@gmail.com>
2016-04-22 21:47:15 -0700

    Booking GUI

--- src/booking/BookingGUI.java
+++ src/booking/BookingGUI.java
@@ -11,8 +11,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 public class BookingGUI {
 	

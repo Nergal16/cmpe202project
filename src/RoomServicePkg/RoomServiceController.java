f8a53f87e1dc11836f630e5a35bd042568506d54
haroldopsf <hubs.filho@gmail.com>
2016-04-24 18:40:25 -0700

    Debugged price decorator and bill print composite functions Coded Egg add on item (was stubbed) Added all buttons to screen

--- src/RoomServicePkg/RoomServiceController.java
+++ src/RoomServicePkg/RoomServiceController.java
@@ -17,6 +17,11 @@ public class RoomServiceController {
     }
     RoomService currentOrder;
     ArrayList<RoomService> burgerList = null;
+    Integer baconCnt = 0;
+    Integer eggCnt = 0;
+    Integer fishCnt = 0;
+    Double bill = 0.0;
+    PriceDecorator billDecorator = null;
 
     private RoomServiceScreen menuScreen = new RoomServiceScreen(this);
 
@@ -37,29 +42,70 @@ public class RoomServiceController {
     }
 
     public void addBacon(){
+        PriceDecorator baconDecorator;
         if (burgerList != null){
-            burgerList.get(burgerList.size() - 1).addChild(new Bacon("Bacon", 3.50, burgerList.get(burgerList.size() - 1)));
+            if (billDecorator == null){
+                baconDecorator = new Bacon("Bacon", 3.50, burgerList.get(burgerList.size() - 1));
+            } else {
+                baconDecorator = new Bacon("Bacon", 3.50, (RoomService) billDecorator);
+            }
+            billDecorator = baconDecorator;
+            burgerList.get(burgerList.size() - 1).addChild((RoomService) baconDecorator);
         }
+        baconCnt++;
     }
 
     public void addEgg(){
+        PriceDecorator eggDecorator;
         if (burgerList != null){
-            burgerList.get(burgerList.size() - 1).addChild(new Bacon("Egg", 2.45, burgerList.get(burgerList.size() - 1)));
+            if (billDecorator == null){
+                eggDecorator = new Egg("Egg", 2.45, burgerList.get(burgerList.size() - 1));
+            } else {
+                eggDecorator = new Egg("Egg", 2.45, (RoomService) billDecorator);
+            }
+            billDecorator = eggDecorator;
+            burgerList.get(burgerList.size() - 1).addChild((RoomService) eggDecorator);
         }
+        eggCnt++;
     }
 
     public void newMarketFish(){
+
         currentOrder.addChild(new WildMarketFish("Alaskan Halibut", 15.5));
+        fishCnt++;
     }
 
-    public void finishOrder(){
+    public Double finishOrder(){
+        bill = 0.0;
         if (burgerList != null){
             for (RoomService burger : burgerList){
                 currentOrder.addChild(burger);
             }
         }
 
+        if (billDecorator != null){
+            bill = billDecorator.getPrice() + (burgerList.size() - 1) * 11.1 + fishCnt * 15.5;
+        } else {
+            if (burgerList != null){
+                bill = burgerList.size() * 11.1;
+            }
+            bill += fishCnt * 15.5;
+        }
+        //Composite prints bill
+        currentOrder.printItems();
+        //Decorator gets prioe
+        System.out.print("Final bill: $" + bill.toString());
+        System.out.println(); System.out.println();
+
+        // clear all billing
+        burgerList = null;
+        billDecorator = null;
+        fishCnt = 0;
+
+        return bill;
     }
+
+
 
 
 }

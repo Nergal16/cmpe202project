49170e88298108e56672aac9287dc76d0974d00a
MUKUL821 <mukul.bajpai0821@gmail.com>
2016-04-27 21:55:47 -0700

    actoy classes added

--- src/booking/Suit.java
+++ src/booking/Suit.java
@@ -4,17 +4,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class FamilyRoom implements RoomVariety{
+public class Suit implements RoomVariety {
 
 	@Override
-	public List<Integer> available() throws SQLException{
-		
+	public List<Integer> available() throws SQLException {
+
 		String query= "Select room_number from availableroom where room_type='FamilyRoom'";
 		ResultSet res= new DatabaseConnector().query(query);
 		while(res.next())
 			list.add(res.getInt("room_number"));
 		return list;
 	}
-
 
 }
